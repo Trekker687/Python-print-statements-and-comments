88bbffd3a5a3245204b0261e624b20a260123e33
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-10-22 17:40:29 +0530

    Add files via upload
    Python print statements and comments

---
+++ PythonPrintStatementsAndComments/Main.py
@@ -0,0 +1,9 @@
+#yay i write comment woooo
+print("This is a print statement")
+print("Hi")
+#Self intro
+print("I like eating food")
+print("I like cars")
+print("I don't have any favourites")
+print("I like physics")
+#Third comment
